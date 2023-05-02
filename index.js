4710101194fd17ee588338f74446d287430fe48b
Abdi Majid <abdi.amajid.dev>
2023-05-02 15:59:21 +0100

    Search Engine

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-require('dotenv').config();
+// require('dotenv').config();
 const APIKY = "2BibzM3DLO1B8XezuR6A5rgHR4wz3gFE";
-
-
 const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=2BibzM3DLO1B8XezuR6A5rgHR4wz3gFE&q=dog&limit=25&offset=0&rating=g&lang=en";
+const btn = document.querySelector("#searchBtn");
+const input = document.querySelector("#searchBar");
+const imageEl = document.querySelector("#imgEl");
+
+
+btn.addEventListener("click", function(){
+    if(input.value !== ""){
+        getImages(input.value)
+    } else{
+        alert("input cant be empty")
+    }
+})
+
+
 
 
 async function getImages(query){
     const endpoint2 = `https://api.giphy.com/v1/gifs/search?api_key=${APIKY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
     // // Print out value of API key stored in .env file
     // console.log(process.env.API_KEY)
-    console.log("hello");
       try {
         const response = await fetch(endpoint2);
         const data = await response.json();
-        randomPick(data.data);
         let url = randomPick(data.data);
-        console.log(url)
+        imageEl.src = url;
+        console.log(data.data)
       } catch (error) {
         console.log(error);
         return Promise.reject(error);
@@ -25,6 +36,7 @@ async function getImages(query){
     function randomPick(data){
         const randomIndex = Math.floor(Math.random() * data.length); 
         console.log(data[randomIndex].url)
+        imageEl.src = data[randomIndex].url;
         return data[randomIndex].url;
     }
     // fetch(endpoint2)
@@ -32,7 +44,6 @@ async function getImages(query){
 // .then((data) => console.log(data));
 // // Print out value of API key stored in .env file
 // console.log(process.env.API_KEY)
-console.log("hello")
 
-    getImages("cat")
+
 
