395026560ec89b4a2389f1f5cc00bf831b451ef8
Abdi Majid <abdi.amajid.dev>
2023-05-02 15:21:37 +0100

    fetch function

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 require('dotenv').config();
+const APIKY = "2BibzM3DLO1B8XezuR6A5rgHR4wz3gFE";
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+
+const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=2BibzM3DLO1B8XezuR6A5rgHR4wz3gFE&q=dog&limit=25&offset=0&rating=g&lang=en";
+
+
+async function getImages(query){
+    const endpoint2 = `https://api.giphy.com/v1/gifs/search?api_key=${APIKY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+    // // Print out value of API key stored in .env file
+    // console.log(process.env.API_KEY)
+    console.log("hello");
+      try {
+        const response = await fetch(endpoint2);
+        const data = await response.json();
+        randomPick(data.data);
+        let url = randomPick(data.data);
+        console.log(url)
+      } catch (error) {
+        console.log(error);
+        return Promise.reject(error);
+      }
+    }
+
+    function randomPick(data){
+        const randomIndex = Math.floor(Math.random() * data.length); 
+        console.log(data[randomIndex].url)
+        return data[randomIndex].url;
+    }
+    // fetch(endpoint2)
+// .then((res) => res.json())
+// .then((data) => console.log(data));
+// // Print out value of API key stored in .env file
+// console.log(process.env.API_KEY)
+console.log("hello")
+
+    getImages("cat")
+
